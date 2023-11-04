c395b105eb2151f183c5c827df2a5db6d1204348
Niclas Nielsen <niclas.nielsen@hotmail.com>
2023-11-04 17:27:57 +0100

    Navbar made

---
+++ .idea/js/navbar.js
@@ -0,0 +1,11 @@
+function loadNavbar() {
+     fetch("navbar.html")
+         .then(response => response.text())
+         .then(html => {
+             document.getElementById("navbar-container").innerHTML = html;
+         })
+         .catch(error => {
+             console.error("Error loading navigation bar:", error);
+         });
+ }
+ window.addEventListener("DOMContentLoaded", loadNavbar);
