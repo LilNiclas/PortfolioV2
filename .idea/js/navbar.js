801d55552d164b5ad7bc7285a30d65e3b24a8058
Niclas Nielsen <niclas.nielsen@hotmail.com>
2023-11-05 20:34:48 +0100

    wrote and create about me, education & experience and most of projects

--- .idea/js/navbar.js
+++ .idea/js/navbar.js
@@ -8,4 +8,29 @@ function loadNavbar() {
              console.error("Error loading navigation bar:", error);
          });
  }
+
+function scrollToSection(targetId) {
+    var targetElement = document.getElementById(targetId);
+    if (targetElement) {
+        window.scrollTo({
+            top: targetElement.offsetTop - 50,
+            behavior: "smooth"
+        });
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    var navLinks = document.querySelectorAll("nav a");
+
+    navLinks.forEach(function(link) {
+        link.addEventListener("click", function(event) {
+            event.preventDefault();
+
+            var targetId = this.getAttribute("href").substring(1);
+
+            scrollToSection(targetId);
+        });
+    });
+});
+
  window.addEventListener("DOMContentLoaded", loadNavbar);
